c89b99058932f648ca358ef78bf88dc055c4f48c
Kashyup Gaud <139047912+kash-gg@users.noreply.github.com>
2025-08-24 12:09:25 +0530

    Time: 37 ms (66.50%) | Memory: 61.7 MB (96.27%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,24 +1,11 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        // Max heap of size k - largest distances at top
-        priority_queue<pair<int, vector<int>>> maxHeap;
-        for (vector<int>& point : points) {
-            int distance = point[0] * point[0] + point[1] * point[1];
-            if (maxHeap.size() < k) {
-                // If heap not full, just add the point
-                maxHeap.push({distance, point});
-            } else if (distance < maxHeap.top().first) {
-                // If current point is closer than the farthest point in heap
-                maxHeap.pop();                   // Remove the farthest point
-                maxHeap.push({distance, point}); // Add current closer point
-            }
-        }
-       vector<vector<int>> result;
-        while (!maxHeap.empty()) {
-            result.push_back(maxHeap.top().second);  // .second is the point
-            maxHeap.pop();
-        }
-        return result; 
+        sort(points.begin(), points.end(),
+            [](const vector<int>& a, const vector<int>& b) {
+                return (a[0]*a[0] + a[1]*a[1]) < (b[0]*b[0] + b[1]*b[1]);
+            });
+        
+        return vector<vector<int>>(points.begin(), points.begin() + k);
     }
 };

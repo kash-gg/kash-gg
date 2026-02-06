7cec94251aee49fcd37438c9a3500acc3f613ebc
Kashyup Gaud <139047912+kash-gg@users.noreply.github.com>
2026-02-06 22:45:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (2.89%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     bool isPowerOfFour(int n) {
         if(n <= 0) return false;
-        
-        double log4n = log2(n) / 2;
-        return log4n == floor(log4n);
+
+        double a = log2(n) / 2;
+        return a == floor(a);
     }
 };

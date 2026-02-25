8a0aa899c817a5fd0320d090aefc07e1db0c182e
Kashyup Gaud <139047912+kash-gg@users.noreply.github.com>
2026-02-25 21:55:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (50.61%) - LeetSync

---
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        if(n<4){
+            return 0;
+        }
+        int count =0;
+        for(int i =5 ;n/i>0;i=i*5){
+            count = count +n/i;
+        }
+        return count;
+    }
+};

eedb5bd9dd7444af60d3b28a5755949dfb3b1d69
Kashyup Gaud <139047912+kash-gg@users.noreply.github.com>
2026-05-04 18:08:19 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (24.68%) - LeetSync

--- 933-increasing-order-search-tree/increasing-order-search-tree.cpp
+++ 933-increasing-order-search-tree/increasing-order-search-tree.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
+    TreeNode* curr;
+
+    void inorder(TreeNode* node) {
+        if (!node) return;
+
+        inorder(node->left);
+
+        curr->right = new TreeNode(node->val);
+        curr = curr->right;
+
+        inorder(node->right);
+    }
+
     TreeNode* increasingBST(TreeNode* root) {
         TreeNode* dummy = new TreeNode(0);
-        TreeNode* curr = dummy;
-
-        function<void(TreeNode*)> inorder = [&](TreeNode* node) {
-            if (!node) return;
-
-            inorder(node->left);
-
-            curr->right = new TreeNode(node->val);
-            curr = curr->right;
-
-            inorder(node->right);
-        };
+        curr = dummy;
 
         inorder(root);
+
         return dummy->right;
     }
 };
